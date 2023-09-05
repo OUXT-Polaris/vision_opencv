b796d6a7d2ca1a484acde667fb60ce6e0ad4d281
Takumi_Yuasa <6228ytaku@gmail.com>
2023-09-05 21:49:40 +0900

    tmp

---
+++ opencv_components/include/opencv_components/opencv_mark_match.hpp
@@ -0,0 +1,42 @@
+// Copyright (c) 2023 OUXT Polaris
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef OPENCV_COMPONENTS__OPENCV_MARK_MATCH_HPP_
+#define OPENCV_COMPONENTS__OPENCV_MARK_MATCH_HPP_
+
+#include <atomic>
+#include <cv_bridge/cv_bridge.hpp>
+#include <image_transport/image_transport.hpp>
+#include <opencv2/opencv.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+#include "opencv_components/visibility_control.h"
+
+namespace match_components
+{
+class OpenCVMatchComponent : public rclcpp::Node
+{
+public:
+  explicit OpenCVMatchComponent(const rclcpp::NodeOptions & options);
+
+  virtual ~OpenCVMatchComponent();
+
+private:
+  rclcpp::Subscription<sensor_msgs::msg::Image> image_sub_;
+  void call_back(const sensor_msgs::msg::Image::ConstSharedPtr image_msg);
+};
+
+}  // namespace opencv_components
+
+#endif  // OPENCV_COMPONENTS__OPENCV_CAMERA_HPP_
---
+++ opencv_components/src/opencv_mark_match.cpp
@@ -0,0 +1,23 @@
+
+#include <opencv_components/opencv_mark_match.hpp>
+namespace match_components
+{
+OpenCVMatchComponent::OpenCVMatchComponent(const rclcpp::NodeOptions & options)
+: rclcpp::Node("opencv_mark_match", options)
+{
+  image_sub_ = create_subscription<sensor_msgs::msg::Image>("camera",1,[this](const auto image){ call_back(image); });//1,引数の型２，トピック名３，バッファサイズ４，関数オブジェクと
+}//ラムダは関数オブジェクト
+
+OpenCVMatchComponent::~OpenCVMatchComponent(){}
+
+void OpenCVMatchComponent::call_back(const sensor_msgs::msg::Image::ConstSharedPtr image_msg)
+{
+  cv_bridge::CvImage bridge;
+  sensor_msgs::msg::Image image = *image_msg.get();
+  bridge.toImageMsg(image);
+
+  bridge.image;// =Mat 
+  
+}
+}
+
