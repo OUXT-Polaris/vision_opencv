daccae4d64b91f4e8163500609f0c4233fee47c5
Masaya Kataoka <ms.kataoka@gmail.com>
2024-03-19 22:15:15 +0900

    fix compile error
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- opencv_components/src/opencv_mark_match.cpp
+++ opencv_components/src/opencv_mark_match.cpp
@@ -1,10 +1,9 @@
 
 #include <ament_index_cpp/get_package_share_directory.hpp>
 #include <opencv_components/opencv_mark_match.hpp>
+#include <rclcpp_components/register_node_macro.hpp>  //
 
-#include <rclcpp_components/register_node_macro.hpp>//
-
-namespace match_components
+namespace opencv_components
 {
 OpenCVMatchComponent::OpenCVMatchComponent(const rclcpp::NodeOptions & options)
 : rclcpp::Node("opencv_mark_match", options),
@@ -80,41 +79,37 @@ void OpenCVMatchComponent::call_back(const sensor_msgs::msg::Image::SharedPtr im
   cv::Canny(img_split[2], dst, 85, 85);
 
   cv::findContours(dst, contours, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
-  
-  for(size_t u = 0;u < contours.size();u++)
-  {
-    for(size_t f = 0;f < triangle_contours.size();f++)
-    {
-      match = cv::matchShapes(contours[u],triangle_contours[f],cv::CONTOURS_MATCH_I1,0);
-      if(parameters_.similarity > match)//近似閾値
+
+  for (size_t u = 0; u < contours.size(); u++) {
+    for (size_t f = 0; f < triangle_contours.size(); f++) {
+      match = cv::matchShapes(contours[u], triangle_contours[f], cv::CONTOURS_MATCH_I1, 0);
+      if (parameters_.similarity > match)  //近似閾値
       {
         matched_contours.push_back(contours[u]);
       }
     }
 
-    for(size_t f = 0;f < circle_contours.size();f++)
-    {
-      match = cv::matchShapes(contours[u],circle_contours[f],cv::CONTOURS_MATCH_I1,0);
-      if(parameters_.similarity > match)//近似閾値
+    for (size_t f = 0; f < circle_contours.size(); f++) {
+      match = cv::matchShapes(contours[u], circle_contours[f], cv::CONTOURS_MATCH_I1, 0);
+      if (parameters_.similarity > match)  //近似閾値
       {
         matched_contours.push_back(contours[u]);
       }
     }
 
-    for(size_t f = 0;f < cross_contours.size();f++)
-    {
-      match = cv::matchShapes(contours[u],cross_contours[f],cv::CONTOURS_MATCH_I1,0);
-      if(parameters_.similarity > match)//近似閾値
+    for (size_t f = 0; f < cross_contours.size(); f++) {
+      match = cv::matchShapes(contours[u], cross_contours[f], cv::CONTOURS_MATCH_I1, 0);
+      if (parameters_.similarity > match)  //近似閾値
       {
         matched_contours.push_back(contours[u]);
       }
     }
   }
 
-  for( size_t f = 0; f < matched_contours.size(); f++ ) {
-    if(parameters_.min_area < cv::contourArea(matched_contours[f]))//最小面積閾値
+  for (size_t f = 0; f < matched_contours.size(); f++) {
+    if (parameters_.min_area < cv::contourArea(matched_contours[f]))  //最小面積閾値
     {
-      if(parameters_.max_area > cv::contourArea(matched_contours[f])){//最大面積閾値
+      if (parameters_.max_area > cv::contourArea(matched_contours[f])) {  //最大面積閾値
         selected_contours.push_back(matched_contours[f]);
       }
     }
@@ -131,8 +126,6 @@ void OpenCVMatchComponent::call_back(const sensor_msgs::msg::Image::SharedPtr im
     cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", drawing).toImageMsg(),
     std::make_shared<sensor_msgs::msg::CameraInfo>(sensor_msgs::msg::CameraInfo()));
 }
-}  // namespace match_components
+}  // namespace opencv_components
 
-RCLCPP_COMPONENTS_REGISTER_NODE(opencv_components::front_left_camera_component)
-RCLCPP_COMPONENTS_REGISTER_NODE(opencv_components::front_right_camera_component)
-RCLCPP_COMPONENTS_REGISTER_NODE(opencv_components::middle_right_camera_component)
+RCLCPP_COMPONENTS_REGISTER_NODE(opencv_components::OpenCVMatchComponent)
